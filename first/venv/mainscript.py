166c59bc1d3ddfbf1b86885e18ed8529cdf5adc6
S22IP110GA14\Uczeń <szynx1989@gmail.com>
2018-04-15 17:58:09 +0200

    ulepszona gra

--- first/venv/mainscript.py
+++ first/venv/mainscript.py
@@ -3,8 +3,19 @@ import random
 import time
 random.seed(time.time())
 komp=['kamien','papier','nozyce']
-check=random.choice(komp)
-wybor=input('Podaj co wybierasz \nkamien papier czy nozyce')
-print('komputer wybrał %s %s' % ( check,gra.gierka(wybor,check)))
+wyjsc=''
+ty=0
+ja=0
+while (wyjsc != 'n'):
+    check=random.choice(komp)
+    wybor=input('Podaj co wybierasz \nkamien papier czy nozyce')
+    wynik = gra.gierka(wybor,check)
+    print('komputer wybrał %s %s' % ( check,wynik))
+    if wynik == 'wygrywasz':
+        ty=ty+1
+    elif  wynik == 'przegrywasz':
+        ja=ja+1
+    print('wynik ty:%s komputer:%s' % (ty, ja))
+    wyjsc = input('Grasz dalej t tak n nie')
 
 
